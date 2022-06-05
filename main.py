6ca37b821d8e5863549fe9eb6a0f15242ffa8a6b
kylekorv3 <korver0311@gmail.com>
2022-06-05 09:11:54 +0800

    simple demo

---
+++ main.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import requests
+import os
+
+# for crawlering crypto logos for demo
+page_url = "https://cryptologos.cc"
+ 
+options = Options()
+options.add_argument("--disable-notifications")
+
+chrome = webdriver.Chrome('./chromedriver', options=options)
+chrome.get(page_url)
+
+soup = BeautifulSoup(chrome.page_source, 'html.parser')
+cols = soup.find("div", {"class": "flex-grid"}).find_all("div", {"class": "col"})
+
+if not os.path.exists("assets"):
+  os.mkdir("assets") 
+
+for col in cols:
+  if (a_tag :=col.find("a")) != None:
+    img_name = a_tag.find("div", {"class": "div-middle-text"}).text.split()[-2][1:-1]
+    img = requests.get(page_url + a_tag.find("img")["src"])
+
+    with open("./assets/" + img_name + ".png", "wb") as file:  
+      file.write(img.content)  
